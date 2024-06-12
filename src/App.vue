31e34bc7d4f7bf2729a40b07ec10fb8165243d2a
Hanne de Deckere <hd.de.deckere@gmail.com>
2024-06-12 08:13:14 +0200

    werkende split

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 
   .container {
     padding: 0;
+    height: 100vh;
   }
   </style>
 
--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,13 +1,25 @@
 <template>
   <v-app>
     <ToolBar :username="username" />
+      <div class="split">
+        <div id="split-0"></div>
+        <div id="split-1"></div>
+        <div id="split-2"></div>
+        <div id="split-3"></div>
+      </div>
   </v-app>
 </template>
 
 <script>
 import ToolBar from './ToolBar.vue'
+import Split from 'split.js'
 
 export default {
+  mounted(){
+    Split(['#split-0', '#split-1', '#split-2', '#split-3'], {
+        minSize: 150,
+    })
+  },
   data() {
     return {
       items: [
@@ -28,3 +40,27 @@ export default {
   },
 };
 </script>
+
+<style>
+.split {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+}
+
+.gutter {
+    background-color: #eee;
+    background-repeat: no-repeat;
+    background-position: 50%;
+}
+
+.gutter.gutter-horizontal {
+    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
+    cursor: col-resize;
+}
+
+/* .v-application--wrap {
+  flex: none;
+  flex-direction: row;
+} */
+</style>
