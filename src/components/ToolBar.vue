ef0b31acbff92f443813294ca8b9fb7513cf984b
segervvv <seger.vanvlijmen@student.hu.nl>
2024-10-24 10:42:04 +0200

    Styling update

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -10,8 +10,10 @@
             </v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item v-for="(item, index) in fileItems" :key="index" :value="index" @click="handleItemClick(item)">
+              <v-list-item-title class="flex"><svg-icon type="mdi" :path="item.icon" class="mr-2"></svg-icon>{{
+                item.title
+              }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -22,8 +24,13 @@
             </v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item v-for="(item, index) in domainItems" :key="index" :value="index"
+              @click="handleItemClick(item)">
+              <v-list-item-title class="flex"><svg-icon v-if="domainStatus === item.title" type="mdi" :path="item.icon"
+                  class="mr-2"></svg-icon>
+                <svg-icon v-else type="mdi" :path="''" class="mr-2"></svg-icon>{{
+                  item.title
+                }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -34,8 +41,11 @@
             </v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item v-for="(item, index) in projectItems" :key="index" :value="index"
+              @click="handleItemClick(item)">
+              <v-list-item-title class="flex"><svg-icon type="mdi" :path="item.icon" class="mr-2"></svg-icon>{{
+                item.title
+              }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -46,8 +56,11 @@
             </v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item v-for="(item, index) in manageItems" :key="index" :value="index"
+              @click="handleItemClick(item)">
+              <v-list-item-title class="flex"><svg-icon type="mdi" :path="item.icon" class="mr-2"></svg-icon>{{
+                item.title
+              }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -75,14 +88,39 @@
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiPlus, mdiFolder, mdiBookmark, mdiClose, mdiRepeatVariant, mdiPencil, mdiHome, mdiCircleSmall } from '@mdi/js'
+
 export default {
   name: 'ToolBar',
   props: ['username'],
+  domainStatus: "HVAC",
   data: () => ({
     items: [
       { title: 'Profile', functionName: 'profile' },
       { title: 'Logout', functionName: 'logout' },
     ],
+    fileItems: [
+      { title: 'New', icon: mdiPlus, functionName: 'fileNew' },
+      { title: 'Open', icon: mdiFolder, functionName: 'fileOpen' },
+      { title: 'Save', icon: mdiBookmark, functionName: 'fileSave' },
+      { title: 'Save as', icon: mdiBookmark, functionName: 'fileSaveAs' },
+      { title: 'Close', icon: mdiClose, functionName: 'fileClose' },
+    ],
+    domainItems: [
+      { title: 'HVAC', icon: mdiCircleSmall, functionName: 'domainHVAC' },
+      { title: 'Assimilation', icon: mdiCircleSmall, functionName: 'domainAssimilation' },
+    ],
+    projectItems: [
+      { title: 'I/O create configuration', icon: mdiRepeatVariant, functionName: 'projectCreateConfig' },
+      { title: 'I/O expand configuration', icon: mdiRepeatVariant, functionName: 'projectEpandConfig' },
+      { title: 'Process coding', icon: '', functionName: 'projectProcessCoding' },
+      { title: 'Group division HS/SS', icon: '', functionName: 'projectGroupDiv' },
+      { title: 'Design rules (Total project)', icon: mdiPencil, functionName: 'projectDesignRules' },
+    ],
+    manageItems: [
+      { title: 'Manage companies', icon: mdiHome, functionName: 'manageCompanies' },
+    ]
   }),
   methods: {
     handleItemClick(item) {
@@ -101,7 +139,16 @@ export default {
       this.$router.replace({ name: 'login' });
       //TODO Log out, are you sure
     },
+    domainHVAC() {
+      this.domainStatus = "HVAC";
+    },
+    domainAssimilation() {
+      this.domainStatus = "Assimilation";
+    }
   },
+  components: {
+    SvgIcon
+  }
 }
 </script>
 
