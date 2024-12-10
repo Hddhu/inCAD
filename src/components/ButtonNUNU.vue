cb82472fc6cc8cea3632c3b719ea68867e20a52e
segervvv <seger.vanvlijmen@student.hu.nl>
2024-12-10 13:30:08 +0100

    Update

--- src/components/ButtonNUNU.vue
+++ src/components/ButtonNUNU.vue
@@ -1,28 +1,30 @@
 <template>
-<div>
-    <div class="bg-primary flex w-fit py-1 px-4 rounded text-xs cursor-pointer" @onclick="this.onClick"><svg-icon v-if="this.icon != null" type="mdi" :path="this.icon" class="mr-2"></svg-icon>{{ this.title }}</div>
-</div>
-  </template>
-  
-  <script>
-  import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiPlus, mdiFolder, mdiBookmark, mdiClose, mdiRepeatVariant, mdiPencil, mdiHome, mdiCircleSmall } from '@mdi/js'
-  
-  export default {
-    name: 'ButtonNUNU',
-    props: ["icon", "title", "onClick"],
-    data: () => ({
-      
-    }),
-    methods: {
-    },
-    components: {
-      SvgIcon
-    }
-  }
-  </script>
-  
-  <style>
+  <div>
+    <div
+      :class='this.class + " buttonBorder border-gray-200 shadow-sm bg-[#f6f6f6] flex w-fit py-1 px-4 rounded text-xs cursor-pointer"'
+      @onclick="this.onClick">{{ this.title }}<svg-icon v-if="this.icon != null" type="mdi" :path="this.icon" class="mr-2" :size="small"></svg-icon></div>
+  </div>
+</template>
 
-  </style>
-  
+<script>
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiPlus, mdiFolder, mdiBookmark, mdiClose, mdiRepeatVariant, mdiPencil, mdiHome, mdiCircleSmall } from '@mdi/js'
+
+export default {
+  name: 'ButtonNUNU',
+  props: ["icon", "title", "onClick", "class"],
+  data: () => ({
+  }),
+  methods: {
+  },
+  components: {
+    SvgIcon
+  }
+}
+</script>
+
+<style>
+.buttonBorder {
+  border-width: 1px
+}
+</style>
--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <ToolBar :username="username" />
-    <splitpanes class="default-theme splitPanes">
-      <pane v-if="!isPaneClosed" :min-size="15" @dblclick="togglePane" class="bg-white">
-        <div class="bg-grey-lighten-2 pl-2 flex justify-between">System library<button @click="togglePane"><svg-icon
+    <splitpanes class="default-theme splitPanes bg-gray-100">
+      <pane v-if="!isPaneClosed" :min-size="15" @dblclick="togglePane" class="bg-white rounded-xl shadow my-3">
+        <div class="p-2 flex justify-between">System library<button @click="togglePane"><svg-icon
               type="mdi" :path="mdiMenuLeftIcon" class="mr-2"></svg-icon></button></div>
         <v-treeview :density="'compact'" v-model:selected="selection" :items="items"
           :select-strategy="'single-independent'" item-value="id" @update:selected="GetInfo(selection)"></v-treeview>
@@ -15,46 +15,57 @@
             @click="togglePane"><svg-icon type="mdi" :path="mdiMenuLeftIcon" class="mt-2 mb-[-4px]"></svg-icon></button>
         </div>
       </pane>
-      <pane class="bg-white" :min-size="15">
-        <div class="bg-grey-lighten-2 pl-2">Project</div>
+      <pane class="bg-white rounded-xl shadow my-3" :min-size="15">
+        <div class="p-2">Project</div>
         <v-treeview :density="'compact'" v-model:selected="selection2" :items="Grandchild"
           :select-strategy="'single-leaf'" item-value="id" return-object></v-treeview>
       </pane>
-      <pane class="bg-white" :min-size="15">
-        <div class="bg-grey-lighten-2 pl-2">System</div>
+      <pane class="bg-white rounded-xl shadow my-3" :min-size="15">
+        <div class="p-2">System</div>
         <SystemPane />
       </pane>
-      <pane class="bg-white h-full" :min-size="25" :max-size="30">
-        <div class="bg-grey-lighten-2 pl-2">System configuration</div>
+      <pane class="bg-white h-full rounded-xl shadow my-3" :min-size="25" :max-size="30">
+        <div class="p-2">System configuration</div>
         <div class="h-full">
           <div class="flex w-full h-fit mb-2">
-            <img class="w-40 m-0"
-              src="https://s3-alpha-sig.figma.com/img/439c/4abe/8df79c909330c5c24c11a8b555740d47?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gXGHtV83YAW3NzrQqlDJj7PPM2T6JH8Tjd4OgBGC4uKvQrMRPimICd3iUI~bSiulCQmtqt568ZhONTuTiwm2HdvemZF~AyJx41TeaK5jWOLsJgfBQdiUZS7Alm03BEqCc2xY~iqBeQTIM3w4K5T-oHj35u6iQCnR0v97ejDBUksrm-oZVOU3Jb5fAQ0mlYrKtaKt6iF3l03509~i83~stn8Nm93EoOHWk-fH-~LOZDzOs1bDqePm5CGVZPFjwl0hGBp6QovVGhK7adBIXZs8odl1V34OI3So49-sHSc8rbdQWXK3j1dCsWY6yZN-rW8LnW7zSfrSw~rPWQ5th---gg__" />
-            <div class="flex flex-col justify-between py-2">
-              <ButtonNUNU :title="'ELEMENT'" />
-              <ButtonNUNU :title="'I/O POINTS'" />
-              <ButtonNUNU :title="'DOCUMENTATION'" />
-              <div>
-                <statusMarker></statusMarker>
-                <ButtonNUNU :title="'STATUS>>'" />
-                <icon></icon>
+
+            <div>
+              <div class=" flex justify-center border-2 border-gray-300 rounded m-3 mt-2 h-24 w-32">
+                <img class="m-0"
+                  src="https://s3-alpha-sig.figma.com/img/439c/4abe/8df79c909330c5c24c11a8b555740d47?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gXGHtV83YAW3NzrQqlDJj7PPM2T6JH8Tjd4OgBGC4uKvQrMRPimICd3iUI~bSiulCQmtqt568ZhONTuTiwm2HdvemZF~AyJx41TeaK5jWOLsJgfBQdiUZS7Alm03BEqCc2xY~iqBeQTIM3w4K5T-oHj35u6iQCnR0v97ejDBUksrm-oZVOU3Jb5fAQ0mlYrKtaKt6iF3l03509~i83~stn8Nm93EoOHWk-fH-~LOZDzOs1bDqePm5CGVZPFjwl0hGBp6QovVGhK7adBIXZs8odl1V34OI3So49-sHSc8rbdQWXK3j1dCsWY6yZN-rW8LnW7zSfrSw~rPWQ5th---gg__" />
               </div>
+              <div class="flex">
+                <ButtonNUNU :title="'Status'" :class="'bg-red-100 border-red-400 ml-3'" />
+                <svg-icon type="mdi" :path="mdiLockOpenIcon" class="ml-4 text-primary"></svg-icon>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between py-2">
+              <ButtonNUNU :title="'Element'" />
+              <ButtonNUNU :title="'I/O Points'" />
+              <ButtonNUNU :title="'Documentation'" />
+              <ButtonNUNU :title="'Motor/control Current'" />
+              <!--  :icon="mdiExportIcon" -->
             </div>
           </div>
           <div class="flex flex-col mx-2 text-sm h-full max-h-[80%]">
             <v-list density="compact" class="mb-2">
-              <v-list-item v-for="(item, index) in selectedItem[0].specs" :key="index" variant="flat">
+              <v-list-item v-for="(item, index) in selectedItem[0].specs.slice(0, 9)" :key="index" variant="flat">
                 <v-list-item-title class="flex w-full justify-between text-sm mr-4">
                   <p>{{ item.title }}</p>
-                  <div class="border-b-[1.5px] border-gray-600 border-solid w-1/2">
+                  <div class="border-b-[1.5px] border-gray-300 w-1/2">
+                    {{ item.value }}</div>
+                </v-list-item-title>
+              </v-list-item>
+
+              <v-divider :opacity="100" class="mb-4 mt-5 mx-4 border-gray-300"></v-divider>
+              <v-list-item v-for="(item, index) in selectedItem[0].specs.slice(9)" :key="index" variant="flat">
+                <v-list-item-title class="flex w-full justify-between text-sm mr-4">
+                  <p>{{ item.title }}</p>
+                  <div class="border-b-[1.5px] border-gray-300 border-solid w-1/2">
                     {{ item.value }}</div>
                 </v-list-item-title>
               </v-list-item>
             </v-list>
-            <ButtonNUNU :title="'MOTOR CURRENT'" />
-            <div class="my-4">
-              <ButtonNUNU :title="'CONTROL CURRENT'" />
-            </div>
           </div>
         </div>
       </pane>
@@ -68,7 +79,7 @@ import ButtonNUNU from '@/components/ButtonNUNU.vue'
 import { Splitpanes, Pane } from 'splitpanes'
 import 'splitpanes/dist/splitpanes.css'
 import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiMenuLeft } from '@mdi/js'
+import { mdiMenuLeft, mdiLockOpen, mdiExport } from '@mdi/js'
 
 export default {
 
@@ -79,6 +90,8 @@ export default {
       selection: [],
       selection2: [],
       mdiMenuLeftIcon: mdiMenuLeft,
+      mdiLockOpenIcon: mdiLockOpen,
+      mdiExportIcon: mdiExport,
       items: [
         {
           id: 1,
@@ -199,11 +212,12 @@ export default {
 
 <style>
 .splitpanes__splitter {
-  background-color: #e0e0e0 !important;
+  background: none !important;
+  border-left: 0 !important;
 }
 
 .splitPanes {
-  height: calc(100vh - 64px);
+  height: calc(100vh - 80px);
 }
 
 .v-list-item--density-compact.v-list-item--one-line {
