623973b26107152befb847c12db54e542463f653
segervvv <seger.vanvlijmen@student.hu.nl>
2024-12-20 12:25:08 +0100

    kleuren fix

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,10 +14,11 @@ import { VTreeview } from 'vuetify/labs/VTreeview'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-    theme: {
+  theme: {
     defaultTheme: 'light',
-      themes: {
-        light: {
+    themes: {
+      light: {
+        colors: {
           primary: '#337AB7',
           primary_lighter_1: '#4089C9',
           primary_darker_1: '#3174AF',
@@ -29,23 +30,11 @@ export default createVuetify({
           error: '#FC3131',
           success: '#4CAF50',
           warning: '#FFC107',
-        },
-        dark: {
-          primary: '#337AB7',
-          primary_lighter_1: '#4089C9',
-          primary_darker_1: '#3174AF',
-          primary_lighter_2: '#5093CE',
-          primary_darker_2: '#2D6A9F',
-          secondary: '#FF4A17',
-          secondary_lighter_1: '#FF5F33',
-          secondary_darker_1: '#FF3F0A',
-          error: '#FC3131',
-          success: '#4CAF50',
-          warning: '#FFC107',
-        },
+        }
       },
     },
+  },
   components: {
     VTreeview,
   },
-  })
+})
